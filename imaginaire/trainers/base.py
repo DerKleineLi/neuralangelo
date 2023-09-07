fix bug for gradient log

imaginaire/trainers/base.py
@@ -484,21 +484,18 @@ class BaseTrainer(object):
         self.timer._time_before_backward()
 
         # For debug
-        # print("I'm before the gradient contribution")
+
         if self.cfg.optim.log_gradient_contribugion:
-            # print("I'm in the gradient contribution")
             self.gradient_contribution = {}
             for loss_name in self.weights:
                 if loss_name in self.losses:
-                    print(f"computing gradient contribution for {loss_name}")
-                    self.scaler.scale(self.losses[loss_name]).backward(
-                        retain_graph=True
+                    grad = torch.autograd.grad(
+                        self.losses[loss_name],
+                        self.model_module.parameters(),
+                        retain_graph=True,
+                        allow_unused=True,
                     )
-                    self.gradient_contribution[loss_name] = aggregate_gradients(
-                        self.model_module
-                    )
-                    print(self.gradient_contribution[loss_name])
-                    self.optim.zero_grad(**self.optim_zero_grad_kwargs)
+                    self.gradient_contribution[loss_name] = aggregate_gradients(grad)
 
         self.scaler.scale(total_loss).backward()
 

projects/nerf/utils/gradient.py
@@ -2,11 +2,11 @@ import torch
 
 
 @torch.no_grad()
-def aggregate_gradients(model):
+def aggregate_gradients(grad):
     sum = 0
     count = 0
-    for param in model.parameters():
-        if param.grad is not None:
-            sum += param.grad.data.abs().sum()
-            count += param.grad.data.numel()
+    for gradient in grad:
+        if gradient is not None:
+            sum += gradient.data.abs().sum()
+            count += gradient.data.numel()
     return (sum / count).detach().cpu().item()